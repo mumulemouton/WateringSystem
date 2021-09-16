b3d19f1e39c97f95a80999dd55c853091f69321f
Anita Tabaczka <atabaczka@edu.cdv.pl>
2021-09-16 23:48:55 +0200

    GND VCC SDA SCL LCD

--- watering_system/watering_system.ino
+++ watering_system/watering_system.ino
@@ -1,6 +1,5 @@
-
-#include <LiquidCrystal.h>
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 const int wet = 653;
 const int dry = 953;
 int K3 = 7;
@@ -8,17 +7,41 @@ int K4 = 6;
 
 void setup() {
   Serial.begin(9600);
-  lcd.begin(16, 2);
+  lcd.init();
+  lcd.backlight();
+  lcd.clear();
   pinMode(K3, OUTPUT);
   pinMode(K4, OUTPUT);
   digitalWrite(K3, HIGH);
   digitalWrite(K4, HIGH);
-  delay(500);
+  delay(1000);
 }
 void loop() {
+  int bazylia = map(analogRead(0), wet, dry, 100, 0);
+  int mieta = map(analogRead(1), wet, dry, 100, 0);
+  if (bazylia < 70) {
+    digitalWrite(K3, LOW);
+  } else {
+    digitalWrite(K3, HIGH); 
+  }
+if (mieta < 70) {
+    digitalWrite(K4, LOW);
+  } else {
+    digitalWrite(K4, HIGH);
+  }
+lcd.init();
+lcd.backlight();
+lcd.clear();
 lcd.setCursor(0,0);
+delay(1000);
+bazylia = map(analogRead(0), wet, dry, 100, 0);
+mieta = map(analogRead(1), wet, dry, 100, 0);
+if(bazylia >= 100)
+bazylia = 100;
+if(mieta >= 100)
+mieta = 100;
+delay(100);
 lcd.print("bazylia: ");
-int bazylia = map(analogRead(0), wet, dry, 100, 0);
 lcd.print(bazylia);
 lcd.print("%");
 Serial.print("bazylia: ");
@@ -26,22 +49,11 @@ Serial.print(bazylia);
 Serial.print("%   ");
 lcd.setCursor(0,1);
 lcd.print("mieta: ");
-int mieta = map(analogRead(1), wet, dry, 100, 0);
 lcd.print(mieta);
 lcd.print("%");
 Serial.print("mieta: ");
 Serial.print(mieta);
 Serial.print("%\n");
-if (bazylia < 70) {
-    digitalWrite(K3, LOW);
-  } else {
-    digitalWrite(K3, HIGH);
-  }
-if (mieta < 70) {
-    digitalWrite(K4, LOW);
-  } else {
-    digitalWrite(K4, HIGH);
-  }
 delay(2000);
 lcd.clear();
 }
